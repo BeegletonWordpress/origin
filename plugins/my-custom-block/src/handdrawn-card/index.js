d06a7a9e2d11dc4d72d7bc371255a15a922d873d
sawa2005 <samuelleeward9@gmail.com>
2026-04-01 10:37:19 +0200

    feat: add multiple card shape options to hand-drawn card block

--- plugins/my-custom-block/src/handdrawn-card/index.js
+++ plugins/my-custom-block/src/handdrawn-card/index.js
@@ -7,7 +7,11 @@ import {
 } from "@wordpress/block-editor";
 import { PanelBody, SelectControl } from "@wordpress/components";
 import metadata from "./block.json";
-import { HAND_DRAWN_CARD_SHAPE } from "../constants";
+import {
+	HAND_DRAWN_CARD_SHAPE,
+	HAND_DRAWN_CARD_SHAPE_2,
+	HAND_DRAWN_CARD_SHAPE_3,
+} from "../constants";
 import { TEMPLATE_BUZZ, TEMPLATE_BUILD, TEMPLATE_BOOST } from "./templates";
 import "../index.css";
 import "./style.css";
@@ -16,11 +20,11 @@ import "./editor.css";
 const BLOCK_CLASSES =
 	"relative flex flex-col w-full h-full !max-w-md min-h-[500px] flex-1 basis-[350px]";
 const WRAPPER_CLASSES =
-	"relative z-10 flex flex-col gap-4 p-10 justify-between flex-1 items-center";
+	"relative z-10 flex flex-col gap-4 p-14 justify-between flex-1 items-center";
 
 registerBlockType(metadata.name, {
 	edit: function Edit({ attributes, setAttributes }) {
-		const { backgroundColor, style, cardLayout } = attributes;
+		const { backgroundColor, style, cardLayout, cardShape } = attributes;
 
 		let customBgColor = style?.color?.background;
 		if (backgroundColor) {
@@ -32,6 +36,13 @@ registerBlockType(metadata.name, {
 			currentTemplate = TEMPLATE_BUILD;
 		} else if (cardLayout === "boost") {
 			currentTemplate = TEMPLATE_BOOST;
+		}
+
+		let currentShape = HAND_DRAWN_CARD_SHAPE;
+		if (cardShape === "shape2") {
+			currentShape = HAND_DRAWN_CARD_SHAPE_2;
+		} else if (cardShape === "shape3") {
+			currentShape = HAND_DRAWN_CARD_SHAPE_3;
 		}
 
 		const blockProps = useBlockProps({
@@ -64,21 +75,38 @@ registerBlockType(metadata.name, {
 							]}
 							onChange={(newLayout) => setAttributes({ cardLayout: newLayout })}
 						/>
+						<SelectControl
+							label="Card Shape"
+							value={cardShape || "shape1"}
+							options={[
+								{ label: "Shape 1", value: "shape1" },
+								{ label: "Shape 2", value: "shape2" },
+								{ label: "Shape 3", value: "shape3" },
+							]}
+							onChange={(newShape) => setAttributes({ cardShape: newShape })}
+						/>
 					</PanelBody>
 				</InspectorControls>
 				<div {...blockProps}>
-					{HAND_DRAWN_CARD_SHAPE}
+					{currentShape}
 					<div {...innerBlocksProps} />
 				</div>
 			</>
 		);
 	},
 	save: function save({ attributes }) {
-		const { backgroundColor, style } = attributes;
+		const { backgroundColor, style, cardShape } = attributes;
 
 		let customBgColor = style?.color?.background;
 		if (backgroundColor) {
 			customBgColor = `var(--wp--preset--color--${backgroundColor})`;
+		}
+
+		let currentShape = HAND_DRAWN_CARD_SHAPE;
+		if (cardShape === "shape2") {
+			currentShape = HAND_DRAWN_CARD_SHAPE_2;
+		} else if (cardShape === "shape3") {
+			currentShape = HAND_DRAWN_CARD_SHAPE_3;
 		}
 
 		const blockProps = useBlockProps.save({
@@ -91,7 +119,7 @@ registerBlockType(metadata.name, {
 
 		return (
 			<div {...blockProps}>
-				{HAND_DRAWN_CARD_SHAPE}
+				{currentShape}
 				<div className={WRAPPER_CLASSES}>
 					<InnerBlocks.Content />
 				</div>
