4e731f1bc79a0d916fe8a236a5ebc381562ed53d
sawa2005 <samuelleeward9@gmail.com>
2026-04-08 14:11:11 +0200

    fix: double padding in edit view

--- plugins/my-custom-block/src/logo-slider/index.js
+++ plugins/my-custom-block/src/logo-slider/index.js
@@ -22,63 +22,60 @@ const TEMPLATE = [
 	["create-block/my-logo-slider-item"],
 ];
 
-registerBlockType(metadata.name, {
-	edit: function Edit({ attributes, setAttributes }) {
-		const { autoplay, scrollSpeed, pauseOnHover } = attributes;
-		const blockProps = useBlockProps({
-			className: "logo-slider-container overflow-hidden",
-		});
-		const innerBlocksProps = useInnerBlocksProps(
-			{ className: BLOCK_CLASSES },
-			{
+	registerBlockType(metadata.name, {
+		edit: function Edit({ attributes, setAttributes }) {
+			const { autoplay, scrollSpeed, pauseOnHover } = attributes;
+			const blockProps = useBlockProps({
+				className: "logo-slider-container overflow-hidden",
+			});
+			const innerBlocksProps = useInnerBlocksProps(blockProps, {
 				allowedBlocks: ALLOWED_BLOCKS,
 				template: TEMPLATE,
 				orientation: "horizontal",
-			},
-		);
+			});
 
-		return (
-			<>
-				<InspectorControls>
-					<PanelBody title="Slider Settings">
-						<ToggleControl
-							label="Autoplay"
-							checked={autoplay}
-							onChange={(val) => setAttributes({ autoplay: val })}
-						/>
-						{autoplay && (
-							<>
-								<RangeControl
-									label="Scroll Speed"
-									value={scrollSpeed}
-									onChange={(val) => setAttributes({ scrollSpeed: val })}
-									min={10}
-									max={200}
-									help="Pixels per second (lower is faster)"
-								/>
-								<ToggleControl
-									label="Pause on Hover"
-									checked={pauseOnHover}
-									onChange={(val) => setAttributes({ pauseOnHover: val })}
-								/>
-							</>
-						)}
-					</PanelBody>
-				</InspectorControls>
+			return (
+				<>
+					<InspectorControls>
+						<PanelBody title="Slider Settings">
+							<ToggleControl
+								label="Autoplay"
+								checked={autoplay}
+								onChange={(val) => setAttributes({ autoplay: val })}
+							/>
+							{autoplay && (
+								<>
+									<RangeControl
+										label="Scroll Speed"
+										value={scrollSpeed}
+										onChange={(val) => setAttributes({ scrollSpeed: val })}
+										min={10}
+										max={200}
+										help="Pixels per second (lower is faster)"
+									/>
+									<ToggleControl
+										label="Pause on Hover"
+										checked={pauseOnHover}
+										onChange={(val) => setAttributes({ pauseOnHover: val })}
+									/>
+								</>
+							)}
+						</PanelBody>
+					</InspectorControls>
 
-				<div {...blockProps}>
-					<div
-						className={innerBlocksProps.className}
-						data-autoplay={autoplay}
-						data-speed={scrollSpeed}
-						data-pause-on-hover={pauseOnHover}
-					>
-						<div {...innerBlocksProps} />
+					<div {...innerBlocksProps}>
+						<div
+							className={BLOCK_CLASSES}
+							data-autoplay={autoplay}
+							data-speed={scrollSpeed}
+							data-pause-on-hover={pauseOnHover}
+						>
+							{innerBlocksProps.children}
+						</div>
 					</div>
-				</div>
-			</>
-		);
-	},
+				</>
+			);
+		},
 	save: function save({ attributes }) {
 		const { autoplay, scrollSpeed, pauseOnHover } = attributes;
 		const blockProps = useBlockProps.save({
