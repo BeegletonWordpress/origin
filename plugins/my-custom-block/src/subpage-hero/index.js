bcf59ead89f1dd1783c8fda43052df681cd3ccea
sawa2005 <samuelleeward9@gmail.com>
2026-04-15 14:10:22 +0200

    fix: correct className syntax for layout direction in subpage hero block

--- plugins/my-custom-block/src/subpage-hero/index.js
+++ plugins/my-custom-block/src/subpage-hero/index.js
@@ -134,7 +134,7 @@ registerBlockType(metadata.name, {
 				</InspectorControls>
 				<div {...blockProps}>
 					<div
-						className={`flex flex-col py-12 mb-8 md:flex-row w-full gap-4 md:gap-2 m-auto md:items-stretch md:min-h-[65vh]${
+						className={`flex flex-col py-12 mb-8 md:flex-row w-full gap-4 md:gap-2 m-auto md:items-stretch md:min-h-[65vh] ${
 							reverseLayout ? "md:flex-row-reverse" : ""
 						}`}
 					>
