11beb4f7174ff8830b1150d51188fa0c41eb492d
sawa2005 <samuelleeward9@gmail.com>
2026-04-20 11:21:30 +0200

    feat: update styles and structure for custom blocks, including footer and hero components

--- plugins/my-custom-block/src/footer/column/index.js
+++ plugins/my-custom-block/src/footer/column/index.js
@@ -1,49 +1,48 @@
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from "@wordpress/blocks";
 import {
 	InnerBlocks,
 	useBlockProps,
 	useInnerBlocksProps,
-} from '@wordpress/block-editor';
-import metadata from './block.json';
-import '../../index.css';
-import './style.css';
-import './editor.css';
+} from "@wordpress/block-editor";
+import metadata from "./block.json";
+import "../../index.css";
+import "./style.css";
+import "./editor.css";
 
-const BLOCK_CLASSES =
-	'flex flex-col gap-4 flex-auto min-w-48 [&_h2]:text-center';
+const BLOCK_CLASSES = "flex flex-col gap-4 flex-0 min-w-48 [&_h2]:text-center";
 const ALLOWED_BLOCKS = [
-	'core/paragraph',
-	'core/list',
-	'core/heading',
-	'core/html',
-	'core/social-links',
+	"core/paragraph",
+	"core/list",
+	"core/heading",
+	"core/html",
+	"core/social-links",
 ];
 const TEMPLATE = [
-	[ 'core/paragraph', { placeholder: 'A column heading' } ],
-	[ 'core/list' ],
+	["core/paragraph", { placeholder: "A column heading" }],
+	["core/list"],
 ];
 
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
 	edit: function Edit() {
-		const blockProps = useBlockProps( {
+		const blockProps = useBlockProps({
 			className: BLOCK_CLASSES,
-		} );
-		const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		});
+		const innerBlocksProps = useInnerBlocksProps(blockProps, {
 			allowedBlocks: ALLOWED_BLOCKS,
 			template: TEMPLATE,
-		} );
+		});
 
-		return <div { ...innerBlocksProps } />;
+		return <div {...innerBlocksProps} />;
 	},
 	save: function save() {
-		const blockProps = useBlockProps.save( {
+		const blockProps = useBlockProps.save({
 			className: BLOCK_CLASSES,
-		} );
+		});
 
 		return (
-			<div { ...blockProps }>
+			<div {...blockProps}>
 				<InnerBlocks.Content />
 			</div>
 		);
 	},
-} );
+});
