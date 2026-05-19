03fca86fabb0a9f12d8fc7a8a47347d77d726a77
sawa2005 <samuelleeward9@gmail.com>
2026-05-19 10:57:59 +0200

    feat: service-block with static test structure added

--- plugins/my-custom-block/my-custom-block.php
+++ plugins/my-custom-block/my-custom-block.php
@@ -54,6 +54,8 @@ function create_block_my_custom_block_block_init() {
     register_block_type( __DIR__ . '/build/count-up-numbers/inner' );
     register_block_type( __DIR__ . '/build/count-up-numbers/item' );
 
+    register_block_type( __DIR__ . '/build/service-block' );
+
     register_post_meta('post', 'team_member_role', [
         'show_in_rest' => true,
         'single' => true,
