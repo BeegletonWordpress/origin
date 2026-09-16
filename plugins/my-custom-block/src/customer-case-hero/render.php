fdc144d0e7814f257b7bf239dbb5bc31e5cdb07a
BeegletonWordpress <wordpress@client.beegleton.com>
2026-09-16 13:22:24 +0200

    Update spacing and width of customer cases

--- plugins/my-custom-block/src/customer-case-hero/render.php
+++ plugins/my-custom-block/src/customer-case-hero/render.php
@@ -108,11 +108,11 @@ $wrapper_attributes = get_block_wrapper_attributes(
 		</div>
 
 		<div class="w-full h-fit max-h-[40vh] flex flex-col justify-start md:justify-center mt-0 max-w-128.75">
-			<p class="mt-4">
+			<div class="customer-case-hero__excerpt mt-4">
 				<?php
-				echo wp_kses_post( get_the_excerpt() );
+				echo wp_kses_post( wpautop( get_the_excerpt() ) );
 				?>
-			</p>
+			</div>
 		</div>
 	</div>
 </div>
